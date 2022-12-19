d09a9cf76a1352d2b8f3983913da7d609e046a03
Daniel <dsalazar219@gmail.com>
2022-12-19 08:46:50 -0600

    in the process of fixing blur on hover with tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,21 @@ import Image from 'next/image';
 
 export default function Home({ shops }) {
 	return (
-		<div className="flex justify-evenly">
+		<div className="flex justify-around flex-wrap ">
 			{shops.map((shop) => (
 				<div
-					className="text-2xl text-center w-1/3 h-auto p-5 border-solid border-white border-2 m-3 "
+					className="relative p-2 rounded-lg shadow-lg bg-white m-2 group"
 					key={shop.id}>
-					<h1>{shop.name}</h1>
 					<Image
+						className="group-hover:blur-lg group-hover:grayscale-60 transition ease-in-out peer"
 						src={`/images/${shop.img}.png`}
 						alt="shop logo"
-						width={500}
-						height={500}
-						layout="responsive"
+						width={400}
+						height={400}
 					/>
+					<p className="text-white text-4xl hidden group-hover:absolute top-1/2 right-0 left-0 z-0 p-0 m-auto w-full h-full group-hover:block">
+						{shop.name}
+					</p>
 				</div>
 			))}
 		</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,11 @@ module.exports = {
 		'./src/components/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
-		extend: {}
+		extend: {
+			grayscale: {
+				60: '60%'
+			}
+		}
 	},
 	plugins: []
 };
