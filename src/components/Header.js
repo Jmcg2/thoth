2de4d4b3dbd860328cefa2745f90fbd4f0994342
Daniel <dsalazar219@gmail.com>
2023-01-04 14:28:25 -0600

    basic styling of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 const Header = () => {
 	return (
 		<>
-			<div className="flex flex-col h-36 w-full bg-white">
-				<h1 className="text-black text-3xl">SorftWair Shoops</h1>
-				<input type="text"></input>
+			<div className="flex flex-col h-36 w-full max-w-screen-2xl mx-auto top-0 sticky z-10 bg-slate-900">
+				<h1 className="text-white text-5xl text-center mt-2">
+					SorftWair Shoops
+				</h1>
+				<div className="w-full my-5 ">
+					<button className="btn absolute left-3">Back</button>
+					<input
+						className="w-1/2 py-2 px-2 rounded text-black absolute right-1/4 focus:outline-none"
+						type="text"></input>
+					<button className="btn absolute right-3">Theme</button>
+				</div>
 			</div>
 		</>
 	);
